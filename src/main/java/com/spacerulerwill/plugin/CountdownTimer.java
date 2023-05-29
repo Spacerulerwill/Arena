e22beb44561c20611ba2bb3008c1a702d87f1544
William Redding <williamdredding@gmail.com>
2023-05-29 13:45:36 +0100

    Changes
    Added: Countdown timer, cage for countdown with sound effects, more fixes

---
+++ src/main/java/com/spacerulerwill/plugin/CountdownTimer.java
@@ -0,0 +1,65 @@
+package com.spacerulerwill.plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Consumer;
+
+public class CountdownTimer implements Runnable{
+
+    private JavaPlugin plugin;
+
+    private Integer assignedTaskID;
+
+    private int seconds;
+    private int secondsLeft;
+
+    // Actions to perform while counting down, before and after
+    private Consumer<CountdownTimer> everySecond;
+    private Runnable afterTimer;
+
+    // Construct a timer
+    public CountdownTimer(JavaPlugin plugin, int seconds,
+                          Runnable afterTimer,
+                          Consumer<CountdownTimer> everySecond) {
+        // Initializing fields
+        this.plugin = plugin;
+
+        this.seconds = seconds;
+        this.secondsLeft = seconds;
+
+        this.afterTimer = afterTimer;
+        this.everySecond = everySecond;
+    }
+    @Override
+    public void run() {
+        // Is the timer up?
+        if (secondsLeft < 1) {
+            // Do what was supposed to happen after the timer
+            afterTimer.run();
+
+            // Cancel timer
+            if (assignedTaskID != null) Bukkit.getScheduler().cancelTask(assignedTaskID);
+            return;
+        }
+
+        // Do what's supposed to happen every second
+        everySecond.accept(this);
+
+        // Decrement the seconds left
+        secondsLeft--;
+    }
+
+    public int getTotalSeconds() {
+        return seconds;
+    }
+
+    public int getSecondsLeft() {
+        return secondsLeft;
+    }
+
+    public void scheduleTimer() {
+        // Initialize our assigned task's id, for later use so we can cancel
+        this.assignedTaskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, this, 0L, 20L);
+    }
+}
--- src/main/java/com/spacerulerwill/plugin/Plugin.java
+++ src/main/java/com/spacerulerwill/plugin/Plugin.java
@@ -2,12 +2,9 @@ package com.spacerulerwill.plugin;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.TextColor;
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.WorldBorder;
+import net.kyori.adventure.title.Title;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,7 +12,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.Random;
 
@@ -27,14 +23,43 @@ public final class Plugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
+    public void tryCreateCageBlock(World world, Location location, Material material){
+        Block block = world.getBlockAt(location);
+        if (block.isEmpty() || block.isLiquid() || !block.isSolid() || block.isPassable() || block.getType() == Material.BARRIER){
+            block.setType(material);
+        }
+    }
+
+    public void createPlayerCage(World world, Location location, Material material) {
+        for (int i = 0; i < 2; i++){
+            Location left = new Location(world, location.getX() - 1.0, location.getY() + i, location.getZ() );
+            Location right = new Location(world, location.getX() + 1.0, location.getY() + i, location.getZ());
+            Location forward = new Location(world, location.getX(), location.getY() + i, location.getZ() + 1.0);
+            Location back = new Location(world, location.getX(), location.getY() + i, location.getZ() - 1.0);
+
+            tryCreateCageBlock(world, left, material);
+            tryCreateCageBlock(world, right, material);
+            tryCreateCageBlock(world, forward, material);
+            tryCreateCageBlock(world, back, material);
+        }
+
+        Location top = new Location(world, location.getX(), location.getY() + 2.0, location.getZ());
+        tryCreateCageBlock(world, top, material);
+
+        Location below = new Location(world, location.getX(), location.getY() - 1.0, location.getZ());
+        tryCreateCageBlock(world, below, material);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         if (command.getName().equalsIgnoreCase("arena")) {
             if (sender instanceof Player p) {
+
+                // command argument handling
                 int borderSize;
 
                 try {
-                    borderSize = Integer.valueOf(args[0]);
+                    borderSize = Integer.parseInt(args[0]);
                 }
                 catch (ArrayIndexOutOfBoundsException e){
                     borderSize = 150;
@@ -52,31 +77,58 @@ public final class Plugin extends JavaPlugin implements Listener {
                 // set world border to radius
                 WorldBorder worldBorder = world.getWorldBorder();
                 worldBorder.setCenter(p.getLocation());
-                worldBorder.setSize(Integer.valueOf(borderSize));
+                worldBorder.setSize(borderSize);
+
+                // set to daytime
+                world.setTime(6000);
+
+                Location newPlayerLocation = p.getLocation();
 
                 // teleport players to random location inside world border
-                // clear inventories
-                // give blindness and slowness
-
                 int half = borderSize / 2;
                 for (Player p_iter : world.getPlayers()) {
+
+                    // calculate new position
                     int x = rand.nextInt(half) - half;
                     int z = rand.nextInt(half) - half;
-                    Location playerLocation = p.getLocation();
-                    Location location = new Location(world, playerLocation.blockX() + x + 0.5, playerLocation.blockY(), playerLocation.blockZ() + z + 0.5);
-                    location = world.getHighestBlockAt(location).getLocation();
-                    p_iter.teleport(location);
-                    p_iter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 500));
-                    p_iter.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 1));
+
+                    // move to new position
+                    newPlayerLocation = new Location(world, newPlayerLocation.blockX() + x, newPlayerLocation.blockY(), newPlayerLocation.blockZ() + z);
+
+                    // find location of highest block on new location
+                    Location highestBlock = world.getHighestBlockAt(newPlayerLocation).getLocation();
+
+                    // move out of the ground and into center of block
+                    newPlayerLocation.set(highestBlock.getX() + 0.5, highestBlock.getY() + 1.0, highestBlock.getZ() + 0.5);
+                    p_iter.teleport(newPlayerLocation);
+
+                    // all the other stuff
                     p_iter.getInventory().clear();
+                    p_iter.setHealth(20.0);
+                    p_iter.setFoodLevel(20);
+                    p_iter.setNoDamageTicks(200); // invulnerable for 10 seconds
+
+                    p_iter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 100, 255)); // cant break blocks
+                    createPlayerCage(world, newPlayerLocation, Material.BARRIER);
                 }
 
-                for (Player p_iter : world.getPlayers()) {
-                    p_iter.teleport(p.getLocation());
-                }
+                // start countdown timer
+                Location finalNewPlayerLocation = newPlayerLocation;
+                CountdownTimer timer = new CountdownTimer(this,
+                        5,
+                        () -> {
+                            p.getServer().showTitle(Title.title(Component.text("§EGo!"), Component.text("")));
+                            createPlayerCage(world, finalNewPlayerLocation, Material.AIR);
+                            world.getPlayers().forEach(p_iter -> p_iter.playSound(p_iter.getLocation(), Sound.ITEM_GOAT_HORN_SOUND_0, 1.0f, 1.0f));
+                        },
+                        (t) -> {
+                            p.getServer().showTitle(Title.title(Component.text("§EGet Ready!"), Component.text("§EStarting in " + t.getSecondsLeft())));
+                            world.getPlayers().forEach(p_iter -> p_iter.playSound(p_iter.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f));
+                        }
+                );
+
+                timer.scheduleTimer();
             }
-        } else {
-
         }
         return true;
     }
